7eed3298f3f8cb8989344c829440c493fbd37190
StephenCranney <Cranney.stephen@gmail.com>
2021-10-23 14:18:49 -0400

    Add files via upload

--- GSS_LDS_Divorce.R
+++ GSS_LDS_Divorce.R
@@ -1,5 +1,7 @@
 library(foreign)
 gss <- read.dta("/Users/stephencranney/Desktop/GSS7218_R3.DTA")
+library(Hmisc)
+library(pollster)
 
 #only take last 15 years. 
 gss <- subset(gss, year > 2003)
@@ -12,13 +14,18 @@ gss$DIVORCE<-ifelse(gss$divorce=="yes" | gss$marital=="divorced" | gss$marital==
 
 #Remove the never marrieds 
 gss<-subset(gss, marital!='never married')
+gss_lds<-subset(gss, other=='mormon')
+
 
 #Summary stats
 aggregate(gss$DIVORCE, list(gss$lds), FUN=mean)
+wtd.mean(gss$DIVORCE, gss$wtssall)
+wtd.mean(gss_lds$DIVORCE, gss_lds$wtssall)
+crosstab(df = gss, x = lds  , y = DIVORCE , weight = wtssall)
 
 #Basic t-test and regressions
 t.test(gss$lds,gss$DIVORCE)
-summary(glm(DIVORCE~ lds+ age, data=gss))
-summary(glm(DIVORCE~ lds+ age + year, data=gss))
-summary(glm(DIVORCE~ lds, data=gss))
+summary(glm(DIVORCE~ lds+ age, data=gss, weights=wtssall))
+summary(glm(DIVORCE~ lds+ age + year, data=gss, weights=wtssall))
+summary(glm(DIVORCE~ lds, data=gss, weights=wtssall))
 
