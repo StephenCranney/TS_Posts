cdb49f2e3235f78aa2b952aed0a016eda2cb7a09
StephenCranney <Cranney.stephen@gmail.com>
2022-09-18 09:15:59 -0400

    Add files via upload

--- FraudLDS.R
+++ FraudLDS.R
@@ -1,7 +1,7 @@
 library(haven)
 
 census <- read_dta('Desktop/U.S. Religion Census Religious Congregations and Membership Study, 2010 (County File).DTA')
-load(file='Desktop/ICPSR_37059/DS0001/37059-0001-Data.rda')
+load(file='Desktop/ICPSR_33523/DS0001/33523-0001-Data.rda')
 
 #Merge
 #census, FIPS, STCODE, CNTYCODE
@@ -10,11 +10,13 @@ table(census$fips)
 table(census$stcode)
 table(census$cntycode)
 
-da37059.0001$stcode<-da37059.0001$FIPS_ST
-da37059.0001$cntycode<-da37059.0001$FIPS_CTY
+da33523.0001$stcode<-da33523.0001$FIPS_ST
+da33523.0001$cntycode<-da33523.0001$FIPS_CTY
 
-table(da37059.0001$FIPS_ST)
-table(da37059.0001$FIPS_CTY)
+table(da33523.0001$FIPS_ST)
+table(da33523.0001$FIPS_CTY)
+
+censuscrime <- merge(da33523.0001,census,by=c("stcode","cntycode"))
 
 #Create a fraud per thousand measure
 table(censuscrime$CPOPARST)
